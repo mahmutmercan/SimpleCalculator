9141633f61789216a41b374e8ab310a5c06ce686
mahmutmercan <56167628+mahmutmercan@users.noreply.github.com>
2020-03-11 18:16:11 +0300

    Last

---
+++ SimpleCalculator/SimpleCalculator/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by İlker isa Mercan on 31.10.2019.
+//  Copyright © 2019 Mahmut Mercan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var firstText: UITextField!
+    
+    @IBOutlet weak var secondText: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+    @IBAction func sumClicked(_ sender: Any) {
+        if let firstNumber = Int(firstText.text!){
+            if  let secondNumber = Int(secondText.text!){
+                let result = firstNumber + secondNumber
+                resultLabel.text = String(result)
+            }
+        }
+        
+    }
+    
+    @IBAction func minusClicked(_ sender: Any) {
+        if let firstNumber = Int(firstText.text!){
+            if  let secondNumber = Int(secondText.text!){
+                let result = firstNumber - secondNumber
+                resultLabel.text = String(result)
+            }
+        }
+        
+    }
+    
+    
+    @IBAction func multiplyClicked(_ sender: Any) {
+        if let firstNumber = Int(firstText.text!){
+            if  let secondNumber = Int(secondText.text!){
+                let result = firstNumber * secondNumber
+                resultLabel.text = String(result)
+            }
+        }
+    }
+    
+    @IBAction func divideClicked(_ sender: Any) {
+        if let firstNumber = Int(firstText.text!){
+            if  let secondNumber = Int(secondText.text!){
+                let result = firstNumber / secondNumber
+                resultLabel.text = String(result)
+            }
+        }
+    }
+    
+}
+
